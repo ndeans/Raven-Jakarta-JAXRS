6dad8258582f5701d579c380555d456f5ee2a667
ndeans <ndeans@hotmail.com>
2024-11-01 14:35:46 -0700

    changes to processor library for RVN-7 necessitated retrofit to Upload data resource.

--- src/main/java/us/deans/jakarta/Upload.java
+++ src/main/java/us/deans/jakarta/Upload.java
@@ -5,9 +5,9 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import us.deans.raven.processor.OppProcessor_02;
-import us.deans.raven.processor.RvnImport;
 import us.deans.raven.processor.Processor;
+import us.deans.raven.processor.OppProcessor;
+import us.deans.raven.processor.RvnImport;
 
 import java.util.Arrays;
 
@@ -19,16 +19,16 @@ public class Upload {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response setPostList(RvnImport upload) {
-        String msg = "Receiving import object... { " + upload.printJob() + " post list: " + upload.getPostCount() + " records }";
+        String msg = "Receiving import object... { " + upload.printJob() + " }";
         logger.info(msg);  // logger.debug(msg);
 
         try {
-            Processor processor = new OppProcessor_02(upload);
-            processor.log();
-            processor.persist();  // activating separately affords a chance to manipulate first)
+            Processor processor = new OppProcessor(upload);
+            // processor.upload(upload);
+            processor.persist();
         } catch (Exception ex) {
             logger.error(ex.toString());
-            logger.error(Arrays.toString(ex.getStackTrace()));
+            // logger.error(Arrays.toString(ex.getStackTrace()));
         }
         return Response.ok(msg).build();
     }
@@ -36,6 +36,6 @@ public class Upload {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getVersion() {
-        return "Raven Jakarta version 1.2";
+        return "Raven Web Service version 1.4";
     }
 }
