9ce7cd743c1d2e92c6ad6ac0e47ad231eb46fcd7
ndeans <ndeans@hotmail.com>
2026-03-09 19:26:37 -0700

    Add Access-Control-Allow-Private-Network header to CORSFilter

--- src/main/java/us/deans/jakarta/CORSFilter.java
+++ src/main/java/us/deans/jakarta/CORSFilter.java
@@ -14,5 +14,6 @@ public class CORSFilter implements ContainerResponseFilter {
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Private-Network", "true");
     }
 }
